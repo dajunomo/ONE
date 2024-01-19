2ed28ca4b24489ec4440ec9e481083084503e088
dajunomo <100373195+dajunomo@users.noreply.github.com>
2024-01-18 23:28:35 -0500

    one

--- 1.iniciando_con_JS/Logica_Programación/app.js
+++ 1.iniciando_con_JS/Logica_Programación/app.js
@@ -1,1 +1,9 @@
+let numeroSecreto = 6;
 let numeroUsuario = prompt("¡Me indicas un numero por favor!:");
+console.log(numeroUsuario);
+
+
+if (numeroUsuario == numeroSecreto) {
+    alert ("acertaste el numero");
+
+};
