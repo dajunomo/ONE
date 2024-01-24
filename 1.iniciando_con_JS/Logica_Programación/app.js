477c41a800fbecdd6532e711795aa2bbadbc447b
dajunomo <100373195+dajunomo@users.noreply.github.com>
2024-01-23 23:11:24 -0500

    update app.js

--- 1.iniciando_con_JS/Logica_Programación/app.js
+++ 1.iniciando_con_JS/Logica_Programación/app.js
@@ -6,4 +6,21 @@ console.log(numeroUsuario);
 if (numeroUsuario == numeroSecreto) {
     alert ("acertaste el numero");
 
+}else{
+    alert ("Lo sentimos, !intenta de nuevo!")
+};
+
+
+let mensajeDeBienvenida = 'Bienvenido a la aplicación';
+alert('mensajeDeBienvenida');
+
+
+
+//javascript
+let contraseniaDelSistema = "contraseniaPrueba!";
+
+let contrasenia = prompt("Ingrese la contraseña del sistema:");
+
+if (contrasenia = contraseniaDelSistema){
+    alert("Acceso al sistema garantizado");
 };
